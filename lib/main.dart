7e37c437992f2df29a34db666b28fe380de102b3
Nyannazz <kantemir.imam@gmail.com>
2019-08-21 23:34:29 +0200

    api callsÄ

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
-
 import 'package:flutter/material.dart';
+import 'package:async/async.dart' as async;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import './video_manager.dart';
 import './search_bar.dart';
 
 void main() => runApp(MyApp());
 
+void getVideo(String url, Function callback) {}
 
-void getVideo(String url, Function callback){
-
-}
-
-class MyApp extends StatefulWidget{
+class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -20,15 +19,26 @@ class MyApp extends StatefulWidget{
 }
 
 class _MyAppState extends State<MyApp> {
-  final Map appState={
-    'loading':false,
-    'error':false,
-    'response': {
-      
-    },
-    };
-  
-  @override 
+  final Map appState = {
+    'loading': false,
+    'error': false,
+    'response': {},
+  };
+
+  Future<String> getVideo() async {
+    setState(() {
+      appState["loading"] = true;
+    });
+    http.Response response = await http.get(Uri.encodeFull(
+        "http://82.165.121.77:5000/simpleinfo?videolink=https://www.youtube.com/watch?v=Kk3hKNNSHqY"));
+    print(response.body);
+    setState(() {
+      appState["loading"] = false;
+      appState["response"] = json.decode(response.body);
+    });
+  }
+
+  @override
   void initState() {
     // TODO: implement initState
     super.initState();
@@ -37,17 +47,32 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple
-      ),
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
       home: Scaffold(
         appBar: SearchBar(
-          searchVideo: (data){
-            print(data);
-            print(appState);
-          },
-          appName: "CoonTube"),
-        body: VideoManager("asd"),
+            searchVideo: (data) {
+              print(data);
+              print(appState);
+              getVideo();
+            },
+            appName: "CoonTube"),
+        body: appState['loading']
+            ? VideoManager("asd")
+            : Card(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Flexible(child: Text(
+                        ((appState["response"].containsKey("title"))
+                            ? appState["response"]["title"]
+                            : "waiting"),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.green)
+                      ),
+                    ),
+                  ],
+                ),
+              ),
       ),
     );
   }
