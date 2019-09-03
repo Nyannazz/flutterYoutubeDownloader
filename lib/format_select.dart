d962f15d4ed2c6023874583d320c2e333ec0cd8f
Nyannazz <kantemir.imam@gmail.com>
2019-09-03 21:03:59 +0200

    format selection finished

--- lib/format_select.dart
+++ lib/format_select.dart
@@ -7,25 +7,64 @@ class FormatSelect extends StatefulWidget {
   _FormatSelectState createState() => _FormatSelectState();
 }
 
-List createRows(List inputList, int selected) {
-  List<DataRow> outputList=[];
-  for (int i = 0; i < inputList.length; i++) {
-    outputList.add(DataRow(
-      /* onSelectChanged: (bool value) {
+class _FormatSelectState extends State<FormatSelect> {
+  int selectedRow = 0;
+
+  List createRows(List inputList, int selected) {
+    List<DataRow> outputList = [];
+    for (int i = 0; i < inputList.length; i++) {
+      outputList.add(DataRow(
+        /* onSelectChanged: (bool value) {
         print(value);
       }, */
-      selected: i==selected,
-      cells: <DataCell>[
-        DataCell(Text(inputList[i]["type"])),
-        DataCell(Text(inputList[i]["quality"])),
-        DataCell(Text(inputList[i]["videoOnly"].toString())),
-      ],
-    ));
+        selected: i == selected,
+        cells: <DataCell>[
+          DataCell(Text(inputList[i]["type"]), onTap: () {
+            setState(() {
+              selectedRow = i;
+            });
+          }),
+          DataCell(
+              (i == selected
+                  ? Center(
+                      child: Container(
+                        color: Colors.lightBlue,
+                        height: 30.0,
+                        child: RaisedButton(
+                          child: Text("SAVE"),
+                          onPressed: (){
+                            
+                          },
+                        ),
+                      ),
+                    )
+                  : Text(inputList[i]["quality"].toString())), onTap: () {
+            setState(() {
+              selectedRow = i;
+            });
+          }),
+          DataCell(
+              (i == selected
+                  ? Center(
+                      child: Container(
+                        color: Colors.greenAccent,
+                        height: 30.0,
+                        child: RaisedButton(
+                          child: Text("DL"),
+                        ),
+                      ),
+                    )
+                  : Text(inputList[i]["videoOnly"].toString())), onTap: () {
+            setState(() {
+              selectedRow = i;
+            });
+          }),
+        ],
+      ));
+    }
+    return outputList;
   }
-  return outputList;
-}
 
-class _FormatSelectState extends State<FormatSelect> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -39,8 +78,7 @@ class _FormatSelectState extends State<FormatSelect> {
               label: Text("only video"),
               numeric: false,
             ),
-          ],
-          rows: createRows(widget.formatList, 3)),
+          ], rows: createRows(widget.formatList, selectedRow)),
     );
   }
 }
